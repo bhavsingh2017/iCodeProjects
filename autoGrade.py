6e50c6c2528a4622977ade2f19fb87b72715b274
bhavsingh2017 <bhavkirs@usc.edu>
2018-10-12 17:39:00 -0700

    processing left

--- autoGrade.py
+++ autoGrade.py
@@ -1,99 +1,30 @@
 import tkinter
 from tkinter import Label
 from tkinter import Entry
+from tkinter import StringVar
 from tkinter import messagebox
 from tkinter import filedialog
 from pathlib import Path
+from tkinter import Button
 from subprocess import Popen, PIPE
 import linecache
-
-
- 
-##get diff
-##TODO
-
-##student file 
-class studentFile:
-	def __init__(self, ID, stuFile, grade):
-		self.ID = ID
-		self.stuFile = stuFile
-		self.grade = grade
-	def getFileName:
-		return self.stuFile
-	def setFileName(x):
-		self.stuFile=x
-	def getID:
-		return self.ID
-	def setID(x):
-		self.ID=x
-	def getGrade:
-		return self.grade
-	def setGrade(x):
-		self.grade = x
-
-
-
-#this function will get rid of spaces on both ends
-#and it will also make every character a lowercase
-#character so there is no mistake with that
-def morphString(myString):
-	myString = myString.lower()
-	##this section gets rid of the spaces from the left to right 
-	i=0
-	while myString[i]==" ":
-		myString=myString[(i+1)::]
-		i=i+1
-
-	##this section gets rid of the spaces from the right to the left
-	i=len(myString)
-	while myString[i]==" ":
-		myString=myString[::(i-1)]
-		i=i-1
-	return myString
-
-def readTextFile(myFile):
-	#read the file here and then return what it is supposed to say
-	#returns the list where the first element is the name of the 
-	#file and the second element is the string of what the output 
-	#should be 
-
-
-	#returns ---> results = [name_of_file, expected_result]
-
-	name_of_file = ""
-	expected_result = ""
-
-	fp = open(myFile, 'r') 
-	name_of_file=fp.readline()
-	with open(myFile) as fp:  
-   		line = fp.readline()
-   		while line:
-   			expected_result = expected_result+line
-       		line = fp.readline()
-	fp.close()
-
-	results= []
-	results.append(name_of_file)
-	results.append(expected_result)
-
-	return results
+import sys
+	            
 
 def returnDifferences(studentFile, asnwerFile):
 	return 1
-
 def getGrade(errors, answer):
 	charCount = len(answer)
 	if(errors >= charCount):
 		return 0
 	else:
 		return 100
-
 def pathException(path):
 	if path!="":
 		print("you picked: "+path)
 	else:
 		print("Exited, please pick a location")
-		return
+	return
 #returns just the fileName & not the path
 def pathToFileName(name):
 	new_name=""
@@ -104,72 +35,168 @@ def pathToFileName(name):
 	##reverse the string
 	return new_name[::-1]
 
+def errorWindow(string):
+	messagebox.showerror('User Error', string)
+	return
+
 # hide main window
 def main():
 
+	#Filetypes
+	ftypes = [
+    	('Python code files', '*.py'), 
+    	('Perl code files', '*.pl;*.pm'),  # semicolon trick
+    	('Java code files', '*.java'), 
+    	('C++ code files', '*.cpp;*.h'),   # semicolon trick
+    	('Text files', '*.txt'), 
+    	('Excel files', '*.csv'),
+    	('All files', '*'), 
+	]
 
-	#this contains three different files, the program asks 
-	#for the csv file, what the result should be text file
-	#and also the folder with all the student files
+
+
+
+
 
 	root = tkinter.Tk()
-	Label(root, text="Enter Output File Name:").grid(row=0)
-	e1 = Entry(root)
-	e1.grid(row=0, column=1)
+	if(len(sys.argv)!=2):
+		print("Invalid Number of Arguments")
+		return
+	outputName = str(sys.argv[1])+".txt"
+	print("The output file is: "+outputName)
 
-	#ask for the csv file 
-	csv_file = filedialog.askopenfilename(initialdir = 
-		"/",title = "Select csv file")
-	#if nothing is selected, exit 
+	root.withdraw()
+
+	x = input("Select csv_file--->Press Enter to Continue")
+	
+	csv_file = filedialog.askopenfilename(title = "Ask for csv file", filetypes=ftypes)
 	pathException(csv_file)
-
-	#here is the filename of the answer file 
+	
+	x = input("Select Answer File--->Press Enter to Continue")
 	root.filename =  filedialog.askopenfilename(initialdir = 
-		"/",title = "Select file",filetypes = (("py files","*.py"),("txt files","*.txt"),
-			("jpeg files","*.jpg"),("all files","*.*")))
-	#if nothing is selected, exit 
+		"/",title = "Select Answer File",filetypes = ftypes)
 	pathException(root.filename)
 
-	#get the folder
-	folder = filedialog.askdirectory(title = "Student Submissions Folder")
+	if csv_file=="" or root.filename=="":
+		errorWindow("Chosen file error, exiting...")
+		return
+	f = open(outputName,"w+")
+
+	x = input("Select Student File Folder--->Press Enter to Continue")
+	folder = filedialog.askdirectory(title = "All Student Submissions For This HW")
 	pathException(folder)
-	pathlist = Path(folder).glob(("all files","*.*"))
-	studentList = []
-
-
-	##create a list of empty student files based on how many files there are 
-	for i in len(pathlist):
-		sampleFile = studentFile("0","empty",0)
-		studentList.append(sampleFile)
-	###
-
-
-	hwName, output = readTextFile(root.filename)
-	
-	print("the filename is: "+hwName)
-	print("and this file contains: ")
-	print(output)
-
-	##going to read throught the csv file here #####
-	#name_of_file=fp.readline()
-	with open(csv_file) as fp:  
-   		line = fp.readline()
-   		while line:
-       		line = fp.readline()
-	fp.close()
+	pathlist = Path(folder).glob('**/*.py')
 
 
 
 
-	##RUN THE ANSWER FILE & STORE OUTPUT HERE###
-	justFileName = pathToFileName(root.filename)
-	print(justFileName)
-	sys.append(root.filename)
-	process = Popen(['python3', justFileName], stdout=PIPE, stderr=PIPE)
-	stdout, stderr = process.communicate()
-	ansOUT = stdout
-	##ansOUT is what the output should be 
-	########################	
+	######----------------------Traverse CSV File----------------------######
+
+
+	#This loop will ignore the file if the submission number is blank*
+	#This loop will add the student to be checked only if they submitted a .py file
+
+
+
+	notPyFile=[]
+	studentIDS = []  #refers to the user id
+	studentSubmissionNumber = [] #refers to the student submission number 
+	key = '"en";"' #this is the part that has the ID
+	getZero = '"ext"":""py"'
+	print("the key is: "+key) 
+	i=2
+	getLine = linecache.getline(csv_file, i)
+	while getLine: #read in all lines
+		#print(getLine)
+		strt = getLine.find(key) #find the key 
+
+		studentGetsZero = False
+		if getLine.find(getZero)==-1: 
+			studentGetsZero=True
+
+		strt = strt+len(key)
+		number = ""
+		ID = ""
+		while getLine[strt]!='"':
+			ID = ID+getLine[strt]
+			strt=strt+1
+		strt=1
+		while getLine[strt]!='"':
+			number = number+getLine[strt]
+			strt=strt+1
+
+		if(studentGetsZero):
+			notPyFile.append(ID)
+			print("Added "+ID+" to pyList*********************************************")
+			print("Student_ID:"+ID)
+			print("Submission:"+number)
+		if(((ID!="" and ID!=";" and ID!=" " and ID!="  ") and studentGetsZero==False)):
+			studentIDS.append(ID)
+			studentSubmissionNumber.append(number)
+			print("Student_ID:"+ID)
+			print("Length:"+str(len(ID)))
+			print("Submission:"+number)
+		i=i+1
+		getLine = linecache.getline(csv_file, i)
+		print()
+		print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+		print()
+
+
+	######----------------------Traverse CSV File----------------------######
+
+	######----------------------Now do the grading----------------------######
+
+	#if another correct submission was not for the same student then
+	#print that the student gets a zero in the output file
+	print("These many students didn't submit .py files: "+str(len(notPyFile)))
+	index = 0
+
+	while index<len(notPyFile):
+		if (notPyFile[index] in studentIDS)==False:
+			f.write("User: "+notPyFile[index]+" gets a 0%\n")
+		index=index+1
+	#empty this list, we don't need it anymore
+	notPyFile[:] = []
+
+	######----------------------Now do the grading----------------------######
+
+
+	print("Submission list size: "+str(len(studentSubmissionNumber)))
+	print("Student_ID list size: "+str(len(studentIDS)))
+
+
+	for i in range(0, len(studentSubmissionNumber)):
+
+		if (int(studentSubmissionNumber[i])<10): #submission is less than ten
+			
+		elif (int(studentSubmissionNumber[i])<=99 and (int(studentSubmissionNumber[i])>=10)): #10-99
+
+		else: #100-999
+
+
+			
+
+	f.close()
+
+	#justFileName = pathToFileName(root.filename)
+	#print(justFileName)
+	#sys.append(root.filename)
+	#process = Popen(['python3', justFileName], stdout=PIPE, stderr=PIPE)
+	#stdout, stderr = process.communicate()
+
+
+	##traverse through all the students solutions in the folder
+	# for path in pathlist:
+	# 	if path == ID:
+	# 		print("student found!")
+	# 	studentString = str(path) #this is the filename of the student
+	# 	studentOutput = ""
+	# 	##RUN THE STUDENT FILE & STORE OUTPUT HERE###
+
+	# 	########################
+	# 	errors = returnDifferences(studentOutput, root.filename)
+		
 
 	# message box display
 main()
